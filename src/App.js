8c82fc62f6f1c291daafbc3fdf6d70104748f8b8
Emmanuel Egomson <fegomson@gmail.com>
2022-07-13 15:25:20 -0500

    Implements voice assistant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
 
-function App() {
+const App = () => {
+  const commands = [
+    {
+      command: 'reset',
+      callback: ({ resetTranscript }) => {
+        resetTranscript();
+      },
+    },
+    {
+      command: 'clear',
+      callback: ({ resetTranscript }) => resetTranscript(),
+    },
+    {
+      command: 'open *',
+      callback: site => {
+        window.open('http://' + site);
+      },
+    },
+    {
+      command: 'increase font size',
+      callback: () => {
+        document.getElementById('content').style.fontSize = '22px';
+      },
+    },
+    {
+      command: 'decrease font size',
+      callback: () => {
+        document.getElementById('content').style.fontSize = '16px';
+      },
+    },
+    {
+      command: 'change text color to *',
+      callback: color => {
+        document.getElementById('content').style.color = color;
+      },
+    },
+  ];
+
+  SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition(
+    {
+      commands,
+    }
+  );
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="nav">
+        <h1>Please Say Something...</h1>
+        <h3>
+          Example: "reset", "clear", "increase font size", "decrease font size"
+        </h3>
+      </div>
+      <div id="content">{transcript}</div>
     </div>
   );
-}
+};
 
 export default App;
